6f30b2887f1faea8faacb2102ffa3cb6bdd7837f
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-19 17:06:51 +0000

    Update login.py

--- app/login.py
+++ app/login.py
@@ -5,48 +5,15 @@ import os, time
 from src import language
 from src import follow_me
 from src import comment_me
-from multiprocessing.pool import ThreadPool
 import time
 import sys
-import random,hashlib,re,threading,json,urllib,datetime
-from requests.exceptions import ConnectionError
-from mechanize import browser
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-
+import random
 
 def jalan(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(random.random() * 0.3)
-
-CorrectUsername = "Selaa"
-CorrectPassword = "Zero"
-
-loop = 'true'
-while (loop == 'true'):
-            os.system('clear')
-            jalan('\033[1;93mfor your username and password you will be transferred automatically')
-    username = raw_input('\033[1;91m>> \x1b[1;91mUsername \x1b[1;91m>> \x1b[1;92m')
-    if (username == CorrectUsername):
-    	password = raw_input('\033[1;91m>> \x1b[1;91mPassword \x1b[1;91m>> \x1b[1;92m')
-        if (password == CorrectPassword):
-            print "Logged in successfully as " + username #SCRIPT FROM INDONESIA
-	    time.sleep(2)
-            loop = 'false'
-        else:
-            print "\033[1;93mPassword Salah"
-            os.system('xdg-open https://www.instagram.com/zero_xvip')
-    else:
-        print "\033[1;94mUsername Salah"
-        os.system('xdg-open https://www.instagram.com/zero_xvip')
-
 
 def loginFb(self, url, config):
 	os.system('clear')
