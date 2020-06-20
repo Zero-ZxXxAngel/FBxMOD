2f7a38496c1884102b0a8978153bbcea1c128cd0
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-20 15:26:36 +0000

    Update login.py

--- app/login.py
+++ app/login.py
@@ -14,10 +14,9 @@ def jalan(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(random.random() * 0.3)
+        time.sleep(random.random() * 0.01)
 
 
-def login():
     os.system('clear')
     passw = getpass.getpass(prompt='TOKEN: ')
 
