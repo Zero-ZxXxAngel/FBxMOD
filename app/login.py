d4ffeccf97472a4b6a7fbf1154468bccf4f567d3
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-20 15:14:09 +0000

    Update login.py

--- app/login.py
+++ app/login.py
@@ -18,11 +18,11 @@ def jalan(s):
 
 
 os.system('clear')
-token = getpass.getpass(prompt='Token: ')
+Token = getpass.getpass(prompt='Token: ')
 
-TKN = {'ZERO0977123'}
+TOKEN = {'ZERO0977123'}
 
-if Token in TKN:
+if Token in TOKEN:
     jalan ("Selamat datang coo")
 else:
     jalan ("TOKEN Salah")
