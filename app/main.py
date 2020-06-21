662544147cd69596737d6a2bc461259c87b53347
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-21 16:10:08 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def jalan(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(random.random() * 0.2)
+        time.sleep(random.random() * 0.02)
 
 class Brute(object):
 	def __init__(self, url):
