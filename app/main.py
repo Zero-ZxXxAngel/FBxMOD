57058fe89d9e3fc0e2bb0c7a3850a1b894e0fca5
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-21 16:06:24 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ class Brute(object):
 			email = str(raw_input("[*]=> Masukan ID Korban : "))
                         passwordlist = str(raw_input("List Password.txt : "))
 
-                        useragents = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+                        useragents = [('User-agent', 'User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 
 
 
