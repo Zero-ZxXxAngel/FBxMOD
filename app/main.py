a3ad9bb46e8abe540ea6252a6993dc1be077682a
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-22 08:10:52 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -61,7 +61,7 @@ class Brute(object):
 		print(self.config.banner())
 		html = parser(response, 'html.parser')
 		jalan('\033[1;91m_________________________________________________________')
-		jalan('\t\t\n\033[1;94m      (\033[0;96m•\033[1;94m) BY: ZERO-ZxXxAngel-ZalasXa \033[0;96m•')
+		jalan('\t\t\n\033[1;94m           (\033[0;96m•\033[1;94m) BY: ZERO-ZxXxAngel-ZalasXa \033[1;94m(\033[0;96m•\033[1;94m')
 		jalan('\033[1;91m_________________________________________________________')
 		print(self.menu)
 		try:
