64ebdd128c637343c16849691c2efdb0881f34fc
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-22 08:27:02 +0000

    Update login.py

--- app/login.py
+++ app/login.py
@@ -20,7 +20,7 @@ def laju(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(random.random() * 0.2)
+        time.sleep(random.random() * 0.05)
 
 
 K1 = "ANONYMOUS-INDONESIA"
