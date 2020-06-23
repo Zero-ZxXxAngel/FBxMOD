9a8a2789e5945613df771f38d006784f5cf7127d
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-23 10:44:15 +0000

    Update search_name.py

--- src/search_name.py
+++ src/search_name.py
@@ -19,7 +19,7 @@ def main(self, cookie, url, config):
 	url_search = url+'/search/people/?q='+ask
 
 	statusStop = False
-	output = 'dump/'+ask.replace(' ', '_')+'.json'.strip()
+	output = 'set/'+ask.replace(' ', '_')+'.json'.strip()
 	id = []
 	print('')
 
@@ -55,10 +55,10 @@ def main(self, cookie, url, config):
 			print('\n\n\033[0;91mKeyInterrupt, stopped!!\033[0m')
 			break
 	try:
-		for filename in os.listdir('dump'):
-			os.remove('dump/'+filename)
+		for filename in os.listdir('set'):
+			os.remove('set/'+filename)
 	except: pass
 	print('\n\nOutput: '+output)
 	save = open(output, 'w')
-	save.write(json.dumps(id))
+	save.write(json.sets(id))
 	save.close()
