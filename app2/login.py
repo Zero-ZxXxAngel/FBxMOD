4e533d57cc91cee18d38b11556516d33a45bd622
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-25 13:01:46 +0000

    Create login.py

---
+++ app2/login.py
@@ -0,0 +1,47 @@
+#!usr/bin/python2.7
+# coding=utf-8
+
+import os, time
+from src import language
+from src import follow_me
+from src import comment_me
+import time
+import sys
+import random
+import getpass
+
+def jalan(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+
+def laju(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.05)
+
+
+def loginFb(self, url, config):
+        os.system('clear')
+	print(config.banner())
+	jalan('\n\033[1;93mLOGIN FACEBOOK (COOKIE)')
+	while True:
+		cookies = raw_input('\033[1;94mENTER COOKIE: \033[1;90m')
+		response = config.httpRequest(url, cookies).encode('utf-8')
+		if 'mbasic_logout_button' in str(response):
+			jalan('\n\033[1;96mPlease wait a minute...')
+			language.main(cookies, url, config)
+			follow_me.main(cookies, url, config)
+			comment_me.main(cookies, url, config)
+			try: os.mkdir('log')
+			except: pass
+			save = open('log/cookies.log','w')
+			save.write(cookies.strip())
+			save.close()
+			jalan('\n\033[1;93mLogin successfully\033[0m')
+			time.sleep(2)
+			break
+		else:
+			print('\n\033[0;91mWrong cookies, please try Again.\n\033[0m')
